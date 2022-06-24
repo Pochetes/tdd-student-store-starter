6f805a3a195bd730301c6d5cb55a9a10c2c54ff3
Roberto Martinez <robertomiguel2001@gmail.com>
2022-06-24 01:31:32 -0700

    action(search/filter): implemented design and rendering of new products onChange, not fully working yet

---
+++ student-store-ui/src/components/SearchFilter/SearchFilter.jsx
@@ -0,0 +1,92 @@
+import React, { useEffect, useState } from "react"
+import "./SearchFilter.css"
+
+const SearchFilter = ({ products, setProducts }) => {
+    const [category, setCategory] = useState("all")
+    const globalProducts = products
+
+    const handleCategory = category => {
+        console.log('category: ', category);
+        console.log('products: ', products);
+        setCategory(category)
+        if (category === "all") {
+            setProducts(products)
+            return
+        }
+
+        let newProducts = []
+        for (let i = 0; i < globalProducts.length; i++) {
+            if (globalProducts[i].category === category) {
+                newProducts.push(globalProducts[i])
+            }
+        }
+
+        setProducts(newProducts)
+    }
+
+    const search = (currentProducts, searchInput) => {
+        let newProducts = []
+        searchInput = searchInput.toLowerCase()
+
+        for (let i = 0; i < currentProducts.length; i++) {
+            let product = currentProducts[i].name.toLowerCase()
+            if (product.search(searchInput) !== -1) {
+                newProducts.push(currentProducts[i])
+            }
+        }
+        return newProducts
+    }
+
+    const handleSearch = e => {
+        let newProducts = []
+        const searchInput = e.target.value
+
+        if (searchInput === "") {
+            setCategory(category)
+            return
+        }
+
+        newProducts = search(products, searchInput)
+        setProducts(newProducts)
+    }
+
+    useEffect(() => {
+        handleCategory("all")
+    }, [])
+
+    return (
+        <div className="search-filter">
+            <div className="search">
+                <input
+                onChange={e => handleSearch(e)}
+                type="search"
+                placeholder="Search Item"
+                />
+            </div>
+            <div className="filter-categories">
+                <div
+                className={`category ${category === "all" && "selected"}`}
+                onClick={() => handleCategory("all")}
+                >All Categories</div>
+                <div
+                className={`category ${category === "clothing" && "selected"}`}
+                onClick={() => handleCategory("clothing")}
+                >Clothing</div>
+                <div
+                className={`category ${category === "food" && "selected"}`}
+                onClick={() => handleCategory("food")}
+                >Food</div>
+                <div
+                className={`category ${category === "accessories" && "selected"}`}
+                onClick={() => handleCategory("accessories")}
+                >Accessories</div>
+                <div
+                className={`category ${category === "tech" && "selected"}`}
+                onClick={() => handleCategory("tech")}
+                >Tech</div>
+            </div>
+        </div>
+    )
+}
+
+export default SearchFilter
